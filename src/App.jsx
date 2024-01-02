f0157362c8d750b79123cecd010bf4a5877c74ed
Brianvera97 <Bryanverac@gmail.com>
2024-01-02 15:30:30 -0300

    feat:update

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import GetList from './Components/GetList/GetList'
+
+
+function App() {
+
+
+  return (
+    <>
+      <GetList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/GetList/GetList.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+import styles from "../GetList/GetList.module.css"
+
+const GetList = () => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const [listItems, setListItems] = useState([])
+
+    const [completedTasks, setCompletedTasks] = useState([]);
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        setListItems([...listItems, inputValue])
+        setInputValue('')
+
+    }
+    const handleDelete = (index) => {
+        const updatedList = listItems.filter((item, i) => i !== index);
+        setListItems(updatedList);
+    };
+
+    const handleCheck = (index) => {
+        const updatedCompletedTasks = [...completedTasks];
+        updatedCompletedTasks[index] = !updatedCompletedTasks[index];
+        setCompletedTasks(updatedCompletedTasks);
+
+
+    }
+
+    return (
+        <>
+            <div >
+                <form className={styles.tabBar} onSubmit={handleSubmit} action="#">
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                    />
+
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+            <div>
+                <ol className={styles.list}>
+                    {listItems.map((item, idx) => (
+                        <li key={idx} className={completedTasks[idx] ? styles.completed : ""} id={styles.outputList}>
+                            {item}
+                            <input type="checkbox" onChange={() => handleCheck(idx)} />
+                            <button onClick={() => handleDelete(idx)}>Delete</button>
+                        </li>
+                    ))}
+                </ol>
+
+            </div>
+        </>
+    )
+}
+export default GetList
